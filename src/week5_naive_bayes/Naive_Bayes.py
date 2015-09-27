b6d8ee4ed1ccfcc1f6094fd0625dcda1e859e25f
pschulz <P.Schulz@uva.nl>
2015-09-27 19:01:44 +0200

    Add naive bayes templates.

--- src/week5_naive_bayes/Naive_Bayes.py
+++ src/week5_naive_bayes/Naive_Bayes.py
@@ -5,6 +5,7 @@ Created on Sep 23, 2015
 '''
 from collections import Counter
 from math import log
+from sys import float_info as fi
 
 class Naive_Bayes(object):
     '''
@@ -16,63 +17,73 @@ class Naive_Bayes(object):
         Constructor
         '''
         self.label_probs = Counter()
+        # A dictionary that maps labels to Counters. Each of these Counters
+        # contains the feature probabilities given the label.
         self.feature_probs = dict()
         self.labels_are_normalised = False
-        self.features_are_normalised = True
+        self.features_are_normalised = False
+        self.unknown_feature = "__UNK__"
         
     def train(self, data, label):
+        '''
+        Train the classifier by counting features in the data set.
+        
+        @param data: A stream of string data from which to extract features
+        @param label: The label of the data 
+        '''
         for line in data:
             self.add_feature_counts(line.split(), label)
-        
-        self.log_normalise_label_probs()
-        self.log_normalise_feature_probs()
     
-    def add_feature_counts(self, data, label):
-        current_feature_counter = None
+    def add_feature_counts(self, features, label):
+        '''
+        Count the features in a feature list.
         
-        if self.label_probs.has_key(label):
-            current_feature_counter = self.feature_probs[label]
-        else:
-            current_feature_counter= Counter()
-            self.feature_counter[label] = current_feature_counter
-            
-        current_feature_counter.update(data)
+        @param features: a list of features.
+        @param label: the label of the data file from which the features were extracted.
+        '''
+        # TODO: implement this!
+        pass 
         
     def update_label_count(self,label):
+        '''
+        Increase the count for the supplied label by 1.
+        
+        @param label: The label whose count is to be increased.
+        '''
         self.label_probs.update([label])
         
     def collapse_infrequent_words(self, threshold):
-        if not self.features_are_normalised:
-            for feature_counter in self.feature_probs.values():
-                for word, count in feature_counter.iteritems():
-                    if count <= threshold:
-                        del feature_counter[word]
-                        feature_counter["__UNK__"] += count
+        '''
+        Sum up the counts of all features whose individual counts are lower than
+        the threshold. Then erase those features. Finally, assign the cumulative 
+        count the unknown feature.
+        
+        @param threshold: The count under which a feature gets collapsed onto the unknown feature. 
+        '''
+        # TODO: Implement this!
             
         
     def log_normalise_label_probs(self):
-        normaliser = float(sum(self.label_probs.values()))
-        for label, count in self.label_probs.iteritems():
-            self.label_probs[label] = log(count/normaliser)
+        '''
+        Normalize the label counts to probabilities and transform them to logprobs.
+        '''
+        # TODO: Implement this!
+        pass
             
     def log_normalise_feature_probs(self):
-        #TODO: check whether it works when we only iterate over values
-        for feature_counter in self.feature_probs.values():
-            normaliser = float(sum(feature_counter()))
-            for feature, count in feature_counter.iteritems():
-                feature_counter[feature] = log(count/normaliser)
+        '''
+        Normalize the feature counts for each label to probabilities and turn them into logprobs.
+        '''
+        # TODO: Implement this!
+        pass
                 
     def predict(self, data):
-        label_scores = list()
-        for label in xrange(self.label_probs):
-            # this is the label prior
-            current_label_score = self.label_probs[label]
-            current_feature_probs = self.feature_probs[label]
-            for line in data:
-                for feature in line.split():
-                    current_label_score += current_feature_probs[feature]
-            
-            label_scores.append(current_label_score)
+        ''' 
+        Predict the most probable label according to the model on a stream of data.
         
-        return label_scores.index(max(label_scores))
+        @param data: A stream of string data from which to extract features
+        @return: the most probable label for the data
+        '''
+        # TODO: implement this!
+        pass 
         
--- src/week5_naive_bayes/naive_bayes_classifier.py
+++ src/week5_naive_bayes/naive_bayes_classifier.py
@@ -6,64 +6,64 @@ Created on Sep 23, 2015
 
 import sys
 import argparse
-from datetime.datetime import now
+from datetime import datetime
 from week5_naive_bayes.Naive_Bayes import Naive_Bayes
-from os import listdir
-from os.path import isfile, isdir, join
+from os import listdir, remove
+from os.path import isfile, join
 
 def train_model(corpus_dir, classifier):
-    print 'Starting training at {}'.format(now())
+    print 'Starting training at {}'.format(datetime.now())
     
-    labels = list()
-    
-    label = 0
     for directory in listdir(corpus_dir):
-        print "Training on label {}".format(label+1)
-        labels.append(directory)
+        print "Training on label {}".format(directory)
         directory_path = join(corpus_dir, directory)
-        for text_file in listdir(dir):
+        for text_file in listdir(directory_path):
             file_path = join(directory_path, text_file)
-            classifier.update_label_count(label)
+            classifier.update_label_count(directory)
             try:
                 with open(file_path) as data_file:
-                    classifier.train(data_file, 0)
+                    classifier.train(data_file, directory)
             except IOError, e:
                 print e 
                 print "It seems that the text_file {} is damaged.".format(text_file)
                 sys.exit(0)
-        label += 1
         
-    print "Finished training at {}".now()
-    return labels
+    classifier.log_normalise_label_probs()
+    classifier.log_normalise_feature_probs()
+    print "Finished training at {}".format(datetime.now())
 
-def make_predictions(test_dir, classifier, labels):
+def make_predictions(predictions_file, test_dir, classifier):
+    if isfile(predictions_file):
+        remove(predictions_file)
+    
     for test_file in listdir(test_dir):
         try:
-            with open(join(test_dir, test_file)) as test, open("predictions.txt", "w") as out:
+            with open(join(test_dir, test_file)) as test, open(predictions_file, "a") as out:
                 prediction = classifier.predict(test)
-                out.write(labels[prediction] +"\n")
+                out.write("{}\n".format(prediction))
         except IOError, e:
             print e 
             print "Something went wrong while reading test file {}".format(test_file)
             sys.exit(0)
 
-def main(files):
+def main():
     
     commandline_parser = argparse.ArgumentParser("Add description.")
     
-    commandline_parser.add_argument("--corpus-directory", nargs =1, help="Specifies the path to the directory where the corpus is stored.")
-    
-    commandline_parser.add_argument("--test-set-directory", nargs=1, help="Specifies the path to the directory where the test files are stored.")
+    commandline_parser.add_argument("--training-corpus-directory", nargs = 1, default=[""], help="Specifies the path to the directory where the corpus is stored.")
+    commandline_parser.add_argument("--test-set-directory", nargs = 1, default=[""], help="Specifies the path to the directory where the test files are stored.")
     
     args = vars(commandline_parser.parse_args())
-    corpus_dir = args["corpus_directory"]
-    test_dir = args["test-set-directory"]
+    corpus_dir = args["training_corpus_directory"][0]
+    test_dir = args["test_set_directory"][0]
     
     nb_classifier = Naive_Bayes()
     
-    labels = train_model(corpus_dir, nb_classifier)
+    if corpus_dir:
+        train_model(corpus_dir, nb_classifier)
     
-    make_predictions(test_dir, nb_classifier, labels)
+    if test_dir:
+        make_predictions("predictions.txt", test_dir, nb_classifier)
 
 if __name__ == '__main__':
     main()
